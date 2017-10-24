54c3295e475be581b98f242b1d7f9cd6cd77a6d1
Tomoyuki Aota <tomoyukia@gmail.com>
2017-10-25 03:51:27 +0900

    Update using jQuery

--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const $ = require('jquery');
+
+$('#node-version').text(process.versions.node);
+$('#chromium-version').text(process.versions.chrome);
+$('#electron-version').text(process.versions.electron);
