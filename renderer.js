e6e9c4df84db4ed0b50ca290a95fd8f6d8444eba
Tomoyuki Aota <tomoyukia@gmail.com>
2017-11-07 02:07:15 +0900

    Extract directory-list-area related code into renderDirectoryListArea method and add directory-tree-area and renderDirectoryTreeArea method.

--- renderer.js
+++ renderer.js
@@ -5,26 +5,38 @@
 const $ = require('jquery');
 const ipc = require('electron').ipcRenderer;
 const dir = require('node-dir');
+const dirTree = require('directory-tree');
 
 $('#node-version').text(process.versions.node);
 $('#chromium-version').text(process.versions.chrome);
 $('#electron-version').text(process.versions.electron);
 
 $('#select-directory-button').on('click', function (event) {
-  ipc.send('open-file-dialog')
+  ipc.send('open-file-dialog');
 });
 
 ipc.on('selected-directory', function (event, selectedPathArray) {
   const selectedPath = selectedPathArray[0];
   $('#selected-directory-area').text(`You selected: ${selectedPath}`);
-  dir.paths(selectedPath, function(err, paths) {
-    if (err) throw err;
-    // console.log('files:\n',paths.files);
-    // console.log('subdirs:\n', paths.dirs);
-    const fileListString = paths.files.map(file => file + '<br>').join('');
-    const fileListAreaHtml = `<p>Files: <br>${fileListString}<p/>`;
-    const subDirListString = paths.dirs.map(dir => dir + '<br>').join('');
-    const subDirListAreaHtml = `<p>Directories: <br>${subDirListString}<p/>`;
-    $('#directory-list-area').html(`${fileListAreaHtml}${subDirListAreaHtml}`);
-  });
+  renderDirectoryTreeArea(selectedPath);
+  renderDirectoryListArea(selectedPath);
 });
+
+function renderDirectoryTreeArea(selectedPath) {
+  const tree = dirTree(selectedPath);
+  //JSON.stringify(tree,undefined,1);
+  console.log(tree);
+}
+
+function renderDirectoryListArea(selectedPath) {
+    dir.paths(selectedPath, function(err, paths) {
+        if (err)
+            throw err;
+        const description = '<p><b>Item List</b></p>'
+        const fileListString = paths.files.map(file => file + '<br>').join('');
+        const fileListAreaHtml = `<p>Files: <br>${fileListString}</p>`;
+        const subDirListString = paths.dirs.map(dir => dir + '<br>').join('');
+        const subDirListAreaHtml = `<p>Directories: <br>${subDirListString}</p>`;
+        $('#directory-list-area').html(`${description}${fileListAreaHtml}${subDirListAreaHtml}`);
+    });
+}
