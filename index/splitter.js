58980734a9acae4b97cb65149e7ec1a19d51457e
Tomoyuki Aota <tomoyukia@gmail.com>
2017-11-09 03:38:15 +0900

    Fix a bug that .content does not fit with the window (instead, blank area appears on the right side) when the window is resized.

--- index/splitter.js
+++ index/splitter.js
@@ -1,14 +1,24 @@
 const $ = require('jquery');
 
+var splitterLocation;
+
 $(document).ready(function(){
   $('#splitter').mousedown(function(e){
     e.preventDefault();
     $(document).mousemove(function(e){
-      $('.content').css("width",$(document).width()-e.pageX+2);
-      $('.side-bar').css("width",e.pageX+2);
+      splitterLocation = e.pageX;
+      $('.content').css("width", $(document).width() - splitterLocation);
+      $('.side-bar').css("width", splitterLocation);
     })
   });
   $(document).mouseup(function(e){
     $(document).unbind('mousemove');
   });
+});
+
+$(window).resize(function() {
+  if(typeof splitterLocation === "undefined") 
+    return;
+    
+  $('.content').css("width", $(document).width() - splitterLocation);
 });
