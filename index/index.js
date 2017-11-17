fdee04c00df28cfd08eb2be3b476b51ea0e79812
Tomoyuki Aota <tomoyukia@gmail.com>
2017-11-18 03:41:43 +0900

    Remove directory list area.

--- index/index.js
+++ index/index.js
@@ -5,7 +5,6 @@
 const fs = require('fs');
 const ipc = require('electron').ipcRenderer;
 const $ = require('jquery');
-const dir = require('node-dir');
 const directoryTree = require('directory-tree');
 
 const treeView = require('./tree-view.js');
@@ -20,7 +19,6 @@ ipc.on('selected-directory', function (event, selectedPathArray) {
   const selectedPath = selectedPathArray[0];
   $('#selected-directory-area').text(`You selected: ${selectedPath}`);
   renderDirectoryTreeArea(selectedPath);
-  renderDirectoryListArea(selectedPath);
   treeView.render(selectedPath);
 });
 
@@ -29,19 +27,6 @@ function renderDirectoryTreeArea(selectedPath) {
   const description = '<p><b>JSON object by directory-tree module</b></p>';
   const stringifiedObject = JSON.stringify(tree, null, 1);
   $('#directory-tree-area').html(`${description}<pre>${stringifiedObject}</pre>`)
-}
-
-function renderDirectoryListArea(selectedPath) {
-    dir.paths(selectedPath, function(err, paths) {
-        if (err)
-            throw err;
-        const description = '<p><b>Item List</b></p>'
-        const fileListString = paths.files.map(file => file + '<br>').join('');
-        const fileListAreaHtml = `<p>Files: <br>${fileListString}</p>`;
-        const subDirListString = paths.dirs.map(dir => dir + '<br>').join('');
-        const subDirListAreaHtml = `<p>Directories: <br>${subDirListString}</p>`;
-        $('#directory-list-area').html(`${description}${fileListAreaHtml}${subDirListAreaHtml}`);
-    });
 }
 
 treeView.onChanged = function(data) {
