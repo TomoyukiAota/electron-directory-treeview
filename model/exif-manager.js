1cede0e57e5f0fabf624ff0f82540476551e5b5f
Tomoyuki Aota <tomoyukia@gmail.com>
2017-11-26 03:23:24 +0900

    Add functionality to get EXIF information from files.

--- model/exif-manager.js
+++ model/exif-manager.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const exifParser = require('exif-parser');
+
+const imageUtility = require('./image-utility');
 
 exports.update = function(directoryTreeRoot) {
   reset();
@@ -12,7 +15,7 @@ function reset() {
 function addRecursively(directoryTreeElementArray) {
   directoryTreeElementArray.forEach(
     (directoryTreeElement) => {
-      add(directoryTreeElement.path);
+      add(directoryTreeElement);
       if(directoryTreeElement.hasOwnProperty("children")) {
         addRecursively(directoryTreeElement.children);
       }
@@ -20,6 +23,21 @@ function addRecursively(directoryTreeElementArray) {
   )
 }
 
-function add() {
-  //TODO
+function add(directoryTreeElement) {
+  if(!imageUtility.isSupportedFilenameExtension(directoryTreeElement.extension))
+    return;
+
+  const bufferLengthRequiredToParseExif = 65635;
+  const readStream = fs.createReadStream(directoryTreeElement.path, {start: 0, end: bufferLengthRequiredToParseExif - 1});
+  readStream.on('readable', () => {
+    let buffer;
+    while (null !== (buffer = readStream.read(bufferLengthRequiredToParseExif))) {
+      console.log(`Fetched ${buffer.length} bytes of data from ${directoryTreeElement.name}`);
+      const exifResult = exifParser.create(buffer).parse();
+      console.log(exifResult);
+    }
+  });
+  readStream.on('end', () => {
+    console.log(`Finished fetching data for ${directoryTreeElement.name}.`);
+  });
 }
