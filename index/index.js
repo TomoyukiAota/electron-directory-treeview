dff594a950e000d4eaa0b4c9159a79f7494d7a0c
Tomoyuki Aota <tomoyukia@gmail.com>
2017-12-12 02:21:48 +0900

    Fix index.js according to ESLint and update .eslintrc.json

--- index/index.js
+++ index/index.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
-const path = require('path'); 
-const ipc = require('electron').ipcRenderer;
+const pathModule = require('path');
+const ipc = require('electron').ipcRenderer; // eslint-disable-line import/no-extraneous-dependencies
 const $ = require('jquery');
 const directoryTree = require('directory-tree');
 
@@ -16,7 +15,7 @@ $('#select-directory-button').on('click', function (event) {
 
 ipc.on('selected-directory', function (event, selectedPathArray) {
   const selectedPath = selectedPathArray[0];
-  $('#selected-directory-area').text(`${path.dirname(selectedPath)}`);
+  $('#selected-directory-area').text(`${pathModule.dirname(selectedPath)}`);
   renderDirectoryTreeArea(selectedPath);
   treeView.update(selectedPath);
 });
@@ -25,27 +24,26 @@ function renderDirectoryTreeArea(selectedPath) {
   const tree = directoryTree(selectedPath);
   const description = '<p><b>JSON object by directory-tree module</b></p>';
   const stringifiedObject = JSON.stringify(tree, null, 1);
-  $('#directory-tree-area').html(`${description}<pre>${stringifiedObject}</pre>`)
+  $('#directory-tree-area').html(`${description}<pre>${stringifiedObject}</pre>`);
 }
 
-treeView.onChanged = function(data) {
+treeView.onChanged = function (data) {
   displaySelectedItems(data);
   updateGoogleMaps(data);
-}
+};
 
 function displaySelectedItems(data) {
   function getHtmlForSelectedNode(node) {
     const fileName = node.text;
-    const path = pathIdPairsHandlerForTreeView.getPath(node.id);
-    return fileName + '<br>';
+    return `${fileName}<br>`;
   }
 
   const selectedNodes = treeView.getSelectedNodes(data);
   const title = '<b>Selected Items</b><br>';
   const itemDescription = (selectedNodes.length === 0)
-    ? "No items are selected."
+    ? 'No items are selected.'
     : selectedNodes.map(node => getHtmlForSelectedNode(node)).join('');
-  $("#selected-items").html(`${title}${itemDescription}`);
+  $('#selected-items').html(`${title}${itemDescription}`);
 }
 
 function updateGoogleMaps(data) {
@@ -59,7 +57,7 @@ function updateGoogleMaps(data) {
       const fileName = node.text;
       const path = pathIdPairsHandlerForTreeView.getPath(node.id);
       const gpsCoordinates = exifManager.getGpsCoordinates(path);
-      return [fileName, gpsCoordinates.latitude, gpsCoordinates.longitude];  
+      return [fileName, gpsCoordinates.latitude, gpsCoordinates.longitude];
     });
 
   googleMapsHander.render(locations);
