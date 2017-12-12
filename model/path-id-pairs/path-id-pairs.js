ca523e24b8f01bd605b5c0057bb4377a456bc149
Tomoyuki Aota <tomoyukia@gmail.com>
2017-12-13 02:35:53 +0900

    Fix path-id-pairs.js according to ESLint and update .eslintrc.json

--- model/path-id-pairs/path-id-pairs.js
+++ model/path-id-pairs/path-id-pairs.js
@@ -1,5 +1,5 @@
-var pairs = [];
-var id = 0;
+let pairs = [];
+let id = 0;
 
 function issueId() {
   return (id++).toString();
@@ -10,66 +10,54 @@ function issueId() {
  * @param {string} path path to register
  * @returns {boolean} false if the path is already registered.
  */
-exports.registerPath = function(path) {
-  if(pairs.some(element => element.path === path))
+exports.registerPath = function (path) {
+  if (pairs.some(element => element.path === path))
     return false;
 
   const newElement = {
     path: path,
-    id: issueId(),
+    id: issueId()
   };
   pairs.push(newElement);
   return true;
-}
+};
 
 /**
  * Clear all registered pairs. After this function call, ID will be issued from 0 again.
  */
-exports.reset = function() {
+exports.reset = function () {
   pairs = [];
   id = 0;
-}
+};
 
 /**
  * Get the ID from the specified path.
  * @param {string} path path
  * @return {string} If path is registered, returns the ID. If not, returns null.
  */
-exports.getId = function(path) {
+exports.getId = function (path) {
   const elements = pairs.filter(element => element.path === path);
   if (elements.length === 1)
     return elements[0].id;
 
-  if(elements.length === 0)
+  if (elements.length === 0)
     return null;
 
-  if(elements.length >= 2)
-  {
-    throw {
-      message: `More than 2 elements are registered for ${path}. See pairs property for all registered elements.`,
-      pairs: pairs
-    };
-  }
-}
+  throw new Error(`More than 2 elements are registered for ${path}.`);
+};
 
 /**
  * Get the path from the specified ID.
  * @param {string} id ID
  * @return {string} If the ID is registered, returns the path. If not, returns null.
  */
-exports.getPath = function(id) {
+exports.getPath = function (id) {
   const elements = pairs.filter(element => element.id === id);
   if (elements.length === 1)
     return elements[0].path;
 
-  if(elements.length === 0)
+  if (elements.length === 0)
     return null;
 
-  if(elements.length >= 2)
-  {
-    throw {
-      message: `More than 2 elements are registered for ${id}. See pairs property for all registered elements.`,
-      pairs: pairs
-    };
-  }
-}
+  throw new Error(`More than 2 elements are registered for ${id}.`);
+};
