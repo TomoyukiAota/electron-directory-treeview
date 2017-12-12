b877d457a5daf24fd8c4ebbe5b64caad29b49710
Tomoyuki Aota <tomoyukia@gmail.com>
2017-12-13 01:35:06 +0900

    Fix splitter.js according to ESLint and update .eslintrc.json

--- index/splitter.js
+++ index/splitter.js
@@ -3,25 +3,25 @@ const $ = require('jquery');
 const contentArea = $('#content');
 const sideBarArea = $('#side-bar');
 const splitter = $('#splitter');
-var splitterLocation;
+let splitterLocation;
 
-$(document).ready(function(){
-  splitter.mousedown(function(e){
+$(document).ready(function () {
+  splitter.mousedown(function (e) {
     e.preventDefault();
-    $(document).mousemove(function(e){
+    $(document).mousemove(function (e) {
       splitterLocation = e.pageX;
-      contentArea.css("width", $(document).width() - splitterLocation);
-      sideBarArea.css("width", splitterLocation);
-    })
+      contentArea.css('width', $(document).width() - splitterLocation);
+      sideBarArea.css('width', splitterLocation);
+    });
   });
-  $(document).mouseup(function(e){
+  $(document).mouseup(function (e) {
     $(document).unbind('mousemove');
   });
 });
 
-$(window).resize(function() {
-  if(typeof splitterLocation === "undefined") 
+$(window).resize(function () {
+  if (typeof splitterLocation === 'undefined')
     return;
-    
-    contentArea.css("width", $(document).width() - splitterLocation);
+
+  contentArea.css('width', $(document).width() - splitterLocation);
 });
