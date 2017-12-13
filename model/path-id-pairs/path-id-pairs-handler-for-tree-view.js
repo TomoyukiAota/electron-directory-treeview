474a9c750d735dd3d32a13510d79ef255ff739f9
Tomoyuki Aota <tomoyukia@gmail.com>
2017-12-14 01:13:36 +0900

    Fix path-id-pairs-handler-for-tree-view.js according to ESLint

--- model/path-id-pairs/path-id-pairs-handler-for-tree-view.js
+++ model/path-id-pairs/path-id-pairs-handler-for-tree-view.js
@@ -1,25 +1,25 @@
 const pathIdPairs = require('./path-id-pairs');
 
-const idPrefix = "tv-";
+const idPrefix = 'tv-';
 
 /**
  * Get the ID for tree view from the specified path.
  * @param {string} path path
  * @return {string} If the path is registered, returns the ID. If not, returns null.
  */
-exports.getIdForTreeView = function(path) {
+exports.getIdForTreeView = function (path) {
   const id = pathIdPairs.getId(path);
   return id === null
     ? null
     : `${idPrefix}${id}`;
-}
+};
 
 /**
  * Get the path from the specified ID for tree view.
  * @param {string} idForTreeView ID for tree view
  * @return {string} If the ID is registered, returns the path. If not, returns null.
  */
-exports.getPath = function(idForTreeView) {
+exports.getPath = function (idForTreeView) {
   const id = idForTreeView.split(idPrefix)[1];
   return pathIdPairs.getPath(id);
-}
+};
