554134d1612ce9344e9ec12ce0cf740d3bb865c5
Tomoyuki Aota <tomoyukia@gmail.com>
2017-12-14 03:12:45 +0900

    Fix exif-manager.js according to ESLint and update .eslintrc.json

--- model/exif-manager.js
+++ model/exif-manager.js
@@ -1,56 +1,57 @@
 const fs = require('fs');
 const exifParser = require('exif-parser');
+const _ = require('lodash');
 
 const imageUtility = require('./image-utility');
 
 const exifPromises = [];
 const pathExifPairs = [];
-const exifFetchError = "Error occured when fetching EXIF."
+const exifFetchError = 'Error occured when fetching EXIF.';
 
 /**
  * Update exif-manager using the specified root of directory tree.
  * @param {*} directoryTreeRoot the root of directory tree
  */
-exports.update = async function(directoryTreeRoot) {
+exports.update = async function (directoryTreeRoot) {
   reset();
   addExifPromiseRecursively([directoryTreeRoot]);
   await Promise.all(exifPromises);
   console.log(pathExifPairs);
-}
+};
 
 /**
  * Get EXIF for the specified file if exists. If not, returns null.
  * @param {string} path file path
  */
-exports.getExif = function(path) {
+exports.getExif = function (path) {
   const pair = pathExifPairs.find(pair => pair.path === path);
   return pair && pair.exif !== exifFetchError
     ? pair.exif
     : null;
-}
+};
 
 /**
  * Get GPS coordinates for the specified file if exists. If not, returns null.
  * @param {string} path file path
  */
-exports.getGpsCoordinates = function(path) {
+exports.getGpsCoordinates = function (path) {
   const exif = exports.getExif(path);
-  if(exif === null)
+  if (exif === null)
     return null;
 
-  if(!exif.hasOwnProperty("tags"))
+  if (!_.has(exif, 'tags'))
     return null;
 
   const tags = exif.tags;
-  const gpsCoordinatesExist = tags.hasOwnProperty("GPSLatitude") && tags.hasOwnProperty("GPSLongitude");
-  if(!gpsCoordinatesExist)
+  const gpsCoordinatesExist = _.has(tags, 'GPSLatitude') && _.has(tags, 'GPSLongitude');
+  if (!gpsCoordinatesExist)
     return null;
 
   return {
     latitude: tags.GPSLatitude,
     longitude: tags.GPSLongitude
-  }
-}
+  };
+};
 
 function reset() {
   exifPromises.length = 0;
@@ -59,42 +60,43 @@ function reset() {
 
 function addExifPromiseRecursively(directoryTreeElementArray) {
   directoryTreeElementArray.forEach(
-    (directoryTreeElement) => {
+    directoryTreeElement => {
       addExifPromise(directoryTreeElement);
-      if(directoryTreeElement.hasOwnProperty("children")) {
+      if (_.has(directoryTreeElement, 'children')) {
         addExifPromiseRecursively(directoryTreeElement.children);
       }
     }
-  )
+  );
 }
 
 function addExifPromise(directoryTreeElement) {
-  if(!imageUtility.isSupportedFilenameExtension(directoryTreeElement.extension))
+  if (!imageUtility.isSupportedFilenameExtension(directoryTreeElement.extension))
     return;
 
-  const promise = 
-    instantiatePromiseToFetchExif(directoryTreeElement)
-    .then(exif => 
+  const promise = instantiatePromiseToFetchExif(directoryTreeElement)
+    .then(exif =>
       pathExifPairs.push({
         path: directoryTreeElement.path,
         exif: exif
       })
     )
-    .catch(() => 
+    .catch(() =>
       pathExifPairs.push({
         path: directoryTreeElement.path,
         exif: exifFetchError
       })
-    )
+    );
 
   exifPromises.push(promise);
 }
 
 function instantiatePromiseToFetchExif(directoryTreeElement) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     let exif;
     const bufferLengthRequiredToParseExif = 65635;
-    const readStream = fs.createReadStream(directoryTreeElement.path, {start: 0, end: bufferLengthRequiredToParseExif - 1});  
+    const readStream = fs.createReadStream(
+      directoryTreeElement.path,
+      {start: 0, end: bufferLengthRequiredToParseExif - 1});
     readStream.on('readable', () => {
       let buffer;
       while (null !== (buffer = readStream.read(bufferLengthRequiredToParseExif))) {
@@ -107,7 +109,7 @@ function instantiatePromiseToFetchExif(directoryTreeElement) {
       }
     });
     readStream.on('end', () => {
-      if(!exif) {
+      if (!exif) {
         reject();
         return;
       }
