d0959c3016ee3438dd3d84d9da68a23d70a703c2
Tomoyuki Aota <tomoyukia@gmail.com>
2017-12-29 20:13:17 +0900

    Refactor info window content generation routine so that onclick event is avialable.

--- src/view/info-window-content-generator.js
+++ src/view/info-window-content-generator.js
@@ -1,16 +1,48 @@
+const dateTimeUtility = require('../model/date-time-utility');
+
 /**
  * Generates the content of info window displayed when a marker is clicked in Google Maps.
  * @param {*} photo photo information
  */
 exports.generate = function (photo) {
-  const thumbnail = photo.thumbnail;
-  const thumbnailArea = thumbnail === null
-    ? 'Thumbnail is not available.'
-    : `<img border="0" src="${thumbnail.dataUrl}" `
-      + `width="${thumbnail.width}" height="${thumbnail.height}"/>`;
-  const dateTime = photo.dateTime || 'Date taken is not available.';
-  const description
-    = `<div style="text-align:center"><b>${photo.name}<b/><div/>`
-    + `<div style="text-align:center"><b>${dateTime}<b/><div/>`;
-  return thumbnailArea + description;
+  const root = document.createElement('div');
+
+  const thumbnailElement = createThumbnailElement(photo);
+  const nameElement = createNameElement(photo);
+  const dateTimeElement = createDateTimeElement(photo);
+
+  [thumbnailElement, nameElement, dateTimeElement]
+    .forEach(element => root.appendChild(element));
+
+  return root;
 };
+
+function createNameElement(photo) {
+  const nameElement = document.createElement('div');
+  nameElement.style.textAlign  = 'center';
+  nameElement.style.fontWeight = 'bold';
+  nameElement.innerText        = photo.name;
+  return nameElement;
+}
+
+function createDateTimeElement(photo) {
+  const dateTime = photo.dateTime || 'Date taken is not available.';
+  const dateTimeElement = document.createElement('div');
+  dateTimeElement.style.textAlign  = 'center';
+  dateTimeElement.style.fontWeight = 'bold';
+  dateTimeElement.innerText        = dateTime;
+  return dateTimeElement;
+}
+
+function createThumbnailElement(photo) {
+  const imgElement = document.createElement('img');
+  imgElement.border  = 0;
+  imgElement.src     = photo.thumbnail.dataUrl;
+  imgElement.width   = photo.thumbnail.width;
+  imgElement.height  = photo.thumbnail.height;
+  imgElement.onclick = event => console.log(dateTimeUtility.getDateTime(photo.path));
+
+  return photo.thumbnail === null
+    ? document.createTextNode('Thumbnail is not available.')
+    : imgElement;
+}
