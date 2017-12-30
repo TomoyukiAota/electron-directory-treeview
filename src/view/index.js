86559c06876d34b065c04ddc78f46338ff0c9052
Tomoyuki Aota <tomoyukia@gmail.com>
2017-12-31 03:40:45 +0900

    Fill with Google Maps and delete debug purpose display. Also, update style in general.

--- src/view/index.js
+++ src/view/index.js
@@ -1,7 +1,6 @@
 const pathModule = require('path');
 const ipc = require('electron').ipcRenderer; // eslint-disable-line import/no-extraneous-dependencies
 const $ = require('jquery');
-const directoryTree = require('directory-tree');
 
 const selectedDirectoryManager = require('../model/selected-directory-manager');
 const treeView = require('./tree-view');
@@ -18,37 +17,14 @@ $('#select-directory-button').on('click', function (event) {
 ipc.on('selected-directory', async function (event, selectedPathArray) {
   const selectedPath = selectedPathArray[0];
   $('#selected-directory-area').text(`${pathModule.dirname(selectedPath)}`);
-  renderDirectoryTreeArea(selectedPath);
   await selectedDirectoryManager.update(selectedPath);
   const dataForJsTree = selectedDirectoryManager.generateDataForJsTree();
   treeView.update(dataForJsTree);
 });
 
-function renderDirectoryTreeArea(selectedPath) {
-  const tree = directoryTree(selectedPath);
-  const description = '<p><b>JSON object by directory-tree module</b></p>';
-  const stringifiedObject = JSON.stringify(tree, null, 1);
-  $('#directory-tree-area').html(`${description}<pre>${stringifiedObject}</pre>`);
-}
-
 treeView.onChanged = function (data) {
-  displaySelectedItems(data);
   updateGoogleMaps(data);
 };
-
-function displaySelectedItems(data) {
-  function getHtmlForSelectedNode(node) {
-    const fileName = node.text;
-    return `${fileName}<br>`;
-  }
-
-  const selectedNodes = treeView.getSelectedNodes(data);
-  const title = '<b>Selected Items</b><br>';
-  const itemDescription = (selectedNodes.length === 0)
-    ? 'No items are selected.'
-    : selectedNodes.map(node => getHtmlForSelectedNode(node)).join('');
-  $('#selected-items').html(`${title}${itemDescription}`);
-}
 
 async function updateGoogleMaps(data) {
   function hasGpsCoordinates(node) {
