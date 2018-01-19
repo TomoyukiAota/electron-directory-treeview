2475a2bfc26a32b48f8529007e2c09dbd31af63f
Tomoyuki Aota <tomoyukia@gmail.com>
2018-01-20 03:26:01 +0900

    Delete unused code related to electron binary location detection.

--- test/e2e-test/environment.js
+++ test/e2e-test/environment.js
@@ -1,34 +1,8 @@
-const os = require('os');
 const pathModule = require('path');
 const Application = require('spectron').Application;
 const electron = require('electron');
 
 const rootDir = pathModule.join(__dirname, '../..');
-
-function getElectronLocation() {
-  const electronModuleDir = pathModule.join(rootDir, 'node_modules/electron');
-  const electronBinaryLocation
-    = pathModule.join(electronModuleDir, getElectronBinaryLocationRelativeToElectronModuleDir());
-  return electronBinaryLocation;
-}
-
-function getElectronBinaryLocationRelativeToElectronModuleDir() {
-  const platform = process.env.npm_config_platform || os.platform();
-
-  switch (platform) {
-    case 'darwin':
-      return 'dist/Electron.app/Contents/MacOS/Electron';
-    case 'freebsd':
-    case 'linux':
-      return 'dist/electron';
-    case 'win32':
-      return 'dist/electron.exe';
-    default:
-      throw new Error(`Electron builds are not available on platform: ${platform}`);
-  }
-}
-
-const electronLocation = getElectronLocation();
 
 function getSpectronApp() {
   return new Application({
